ab6b2172e8c9fe869d9996799b5354316b2230b6
usametov <usametov@gmail.com>
2017-02-19 00:36:17 +0600

    add tests

---
+++ TagSortService.Tests/BookmarkCollectionRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Bookmarks.Common;
+using Nancy;
+using Nancy.Testing;
+using Xunit;
+
+namespace TagSortService.Tests
+{
+    public class BookmarkCollectionRepositoryTests
+    {
+        [Fact]
+        public void Should_answer_200_onRootPath()
+        {
+            var sut = new Browser(new DefaultNancyBootstrapper());
+
+            var actual = sut.Get("/");
+
+            Assert.Equal(HttpStatusCode.OK, actual.StatusCode);
+        }
+
+        [Fact]
+        public void Shoud_get_recycling_bookmarks()
+        {
+            var sut = new Browser(new DefaultNancyBootstrapper());
+
+            var actual = sut.Get("/bookmarksByTagBundle/recycling/0/3000/");
+
+            Assert.True(actual.Body.DeserializeJson<Bookmark[]>().Length > 0);
+        }
+    }
+}
