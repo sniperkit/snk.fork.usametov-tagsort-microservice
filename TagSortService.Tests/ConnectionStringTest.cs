af790ffefc8177b5fd29d3728c85d7ed514a80f4
usametov <usametov@gmail.com>
2017-02-19 22:28:29 +0600

    prep for cloud hosting

---
+++ TagSortService.Tests/ConnectionStringTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TagSortService.Tests
+{
+    public class ConnectionStringTest
+    {
+        [Fact]
+        public void Should_get_appHarbor_connection_string()
+        {
+            Assert.Equal(Utils.GetConnectionString(), "test_connect_string");
+        }
+    }
+}
