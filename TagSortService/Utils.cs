d8281145524f88b0642169741d1b0346410480d0
usametov <usametov@gmail.com>
2017-02-20 00:23:09 +0600

    add maybe

--- TagSortService/Utils.cs
+++ TagSortService/Utils.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using Bookmarks.Common;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace TagSortService
 {
@@ -8,23 +10,54 @@ namespace TagSortService
     {
         public const string UNDEFINED = "undefined";
 
+        /// <summary>
+        /// this will throw InvalidOperationException in case if connection string has not been set up
+        /// </summary>
+        /// <returns></returns>
         public static string GetConnectionString()
-        {            
-            ConnectionStringsSection section =
-                ConfigurationManager.GetSection("connectionStrings") as ConnectionStringsSection;
+        {
+            #region old code
+            //ConnectionStringsSection section =
+            //    ConfigurationManager.GetSection("connectionStrings") as ConnectionStringsSection;
 
-            return
-                   !string.IsNullOrEmpty(ConfigurationManager.AppSettings["MONGOLAB_URI"]) ?
-                   ConfigurationManager.AppSettings["MONGOLAB_URI"] :
-                   !string.IsNullOrEmpty(ConfigurationManager.AppSettings["MONGOHQ_URL"]) ?
-                   ConfigurationManager.AppSettings.Get("MONGOHQ_URL") :
-                   section.ConnectionStrings.Count > 0 ?
-                   section.ConnectionStrings[0].ConnectionString : null;
+            //return
+            //       !string.IsNullOrEmpty(ConfigurationManager.AppSettings["MONGOLAB_URI"]) ?
+            //       ConfigurationManager.AppSettings["MONGOLAB_URI"] :
+            //       !string.IsNullOrEmpty(ConfigurationManager.AppSettings["MONGOHQ_URL"]) ?
+            //       ConfigurationManager.AppSettings.Get("MONGOHQ_URL") :
+            //       section.ConnectionStrings.Count > 0 ?
+            //       section.ConnectionStrings[0].ConnectionString : null;
+            #endregion
+
+            return GetConfig().First(s => s.IsSomething()).Value;
         }
 
         public static string[] ToStringArray(this TagCount[] tagCounts) 
         {
             return tagCounts.Select(t => t.Tag).ToArray();
+        }
+
+        public static Maybe<string> GetMaybeFromString(this string s)
+        {
+            return (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+                ? Maybe<string>.Nothing
+                : s.ToMaybe();
+        }
+
+        public static IEnumerable<Maybe<string>> GetConfig()
+        {
+            ConnectionStringsSection section =
+                ConfigurationManager.GetSection("connectionStrings") as ConnectionStringsSection;
+                        
+            return new List<Maybe<string>> {
+                ConfigurationManager.AppSettings["MONGOLAB_URI"].GetMaybeFromString()
+            ,
+                ConfigurationManager.AppSettings.Get("MONGOHQ_URL").GetMaybeFromString()
+            ,
+                section.ConnectionStrings.Count > 0 
+                ? section.ConnectionStrings[0].ConnectionString.GetMaybeFromString() 
+                : Maybe<string>.Nothing
+              };            
         }
     }
 }
