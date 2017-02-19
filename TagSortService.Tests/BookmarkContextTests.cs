cfab6d7fa5baad98ff4313729dc9fb6295505c73
usametov <usametov@gmail.com>
2017-02-19 16:26:01 +0600

    add IoC for bookmarksContext

---
+++ TagSortService.Tests/BookmarkContextTests.cs
@@ -0,0 +1,52 @@
+﻿using Bookmarks.Common;
+using FakeItEasy;
+using Nancy;
+using Nancy.Testing;
+using System.Collections.Generic;
+using Xunit;
+
+namespace TagSortService.Tests
+{
+    public class BookmarkContextTests
+    {
+        private readonly IBookmarksContext fakeBookmarksContext;
+        private Browser sut;
+        //private readonly Bookmark bookmarks;
+        private readonly TagBundle tagBundle;
+
+        public BookmarkContextTests()
+        {
+            fakeBookmarksContext = A.Fake<IBookmarksContext>();
+            sut = new Browser(with =>
+            {
+                with.Dependency(fakeBookmarksContext);
+                with.Module<BookmarkCollectionRepository>();
+            });
+
+            tagBundle = new TagBundle { Name = "linux" };
+            //bookmarks = new Bookmark()
+            //{
+            //    Id = "57146c5f083989dcf1e69c4c"
+            //    , LinkText = "LinuxCNC"
+            //    , LinkUrl = "http://www.linuxcnc.org/"
+            //    , Tags = new List<string>() { "cnc", "3dprinting", "linux", "opensource" }
+            //};
+        }
+
+        [Fact]
+        public void Should_call_GetBookmarksByTagBundle()
+        {
+            sut.Get(string.Format("/bookmarksByTagBundle/{0}/0/3/", tagBundle.Name));
+
+            AssertCalledBookmarksContext(tagBundle);
+        }
+
+        private void AssertCalledBookmarksContext(TagBundle expected)
+        {
+            A.CallTo(() =>
+              fakeBookmarksContext.GetBookmarksByTagBundle(expected.Name
+                , 0, 3))
+              .MustHaveHappened();
+        }
+    }
+}
---
+++ TagSortService/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Bookmarks.Common;
+using Nancy;
+using Nancy.TinyIoc;
+
+namespace TagSortService
+{
+    public class BookmarksBootstrapper : DefaultNancyBootstrapper
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return Utils.GetConnectionString();
+            }
+        }
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            var context = new Bookmarks.Mongo.Data.BookmarksContext(ConnectionString);
+            container.Register<IBookmarksContext>(context);
+        }
+    }
+}
